371a6f8c84a2142280246d93cd8b911e4eed4741
Bhavya Dhanuka <kesudhanuka@gmail.com>
2024-10-11 18:03:24 +0530

    Added functionalities to the textview in the menu items.

--- app/src/main/java/com/example/loginapp/MenuActivity.kt
+++ app/src/main/java/com/example/loginapp/MenuActivity.kt
@@ -1,10 +1,17 @@
 package com.example.loginapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MenuActivity: AppCompatActivity() {
+    private lateinit var profileTextView: TextView
+    private lateinit var logoutTextView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu_page)
@@ -13,6 +20,17 @@ class MenuActivity: AppCompatActivity() {
         val closeButton: View = findViewById(R.id.close_button)
         closeButton.setOnClickListener {
             finish() // Close this activity when the close button is clicked
+        }
+        profileTextView = findViewById(R.id.profile_btn)
+        profileTextView.setOnClickListener {
+            val intent = Intent(this, ProfilePageActivity::class.java)
+            startActivity(intent)
+        }
+        logoutTextView=findViewById(R.id.logout_btn)
+        logoutTextView.setOnClickListener{
+            Toast.makeText(this, getString(R.string.logout_successful), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
     // Handle back button press to finish the activity
