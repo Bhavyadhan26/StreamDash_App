370858191bc6d704a2f05d423d19c6a7a8f046f4
Aman Phatak <aman@zeffo.me>
2024-10-14 08:41:28 +0530

    feat: rename sensors

--- scripts/fcm/fcm/__main__.py
+++ scripts/fcm/fcm/__main__.py
@@ -1,3 +1,4 @@
+import random
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
@@ -5,22 +6,22 @@ from firebase_admin import firestore
 
 
 sample_data = (
-    ("Temperature", 10),
-    ("Humidity", 20),
-    ("Ultrasonic", 30),
-    ("Hydrogen", 40),
-    ("Motion", 50),
-    ("Ambient Light", 60),
-    ("Not Really A", 70),
-    ("Chocolate", 80),
-    ("Crystal Methamphetamine", 90),
-    ("Shiny Pokemon", 100),
-    ("Heat", 110),
-    ("Supercalifragilisticexpialidocious", 120),
-    ("Incorrect Spelling", 130),
-    ("Gay", 140),
-    ("Words That Are Over Five Hundred Characters", 501),
-    ("Number of Minecraft Servers in Rust", 9999999999999),
+    "Temperature",
+    "Humidity",
+    "Ultrasonic",
+    "Hydrogen",
+    "Motion",
+    "Ambient Light",
+    "Hall Effect",
+    "Color",
+    "Vibration",
+    "Tilt",
+    "Heat",
+    "Pressure",
+    "Touch",
+    "Heartbeat",
+    "Moisture",
+    "Smoke",
 )
 
 cred = credentials.Certificate("./service_account.json")
@@ -32,7 +33,8 @@ doc = store.collection("users").document(username).get()
 token = doc.to_dict().get("deviceToken")
 # See documentation on defining a message payload.
 
-for sensor, value in sample_data:
+for sensor in sample_data:
+    value = random.randint(0, 1024)
     message = messaging.Message(
         data={
             "sensor": sensor,
